8d65db71935c3c2ed8cca9b8323318731b3d422d
sudarshan-mn <sudarshan.mn2004@gmail.com>
2026-02-02 19:46:16 +0530

    added healthController

--- src/main/java/com/company/complaintsystem/config/SecurityConfig.java
+++ src/main/java/com/company/complaintsystem/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
-                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/auth/**","/health").permitAll()
 
                 .anyRequest().authenticated()
             )
---
+++ src/main/java/com/company/complaintsystem/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.company.complaintsystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
